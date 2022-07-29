1b474bc702ed738531042a85bebe1948f58f7ce6
Maksym <mmajor143@gmail.com>
2022-07-29 14:18:38 +0300

    Add Mapper

---
+++ ePicture.BLL/Infrastructure/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using ePicture.BLL.DTO;
+using ePicture.DAL.Entities;
+
+namespace ePicture.BLL.Infrastructure.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>()
+                .ForMember(x => x.Name, z => z.MapFrom(z => z.Name))
+                .ForMember(x => x.Surname, z => z.MapFrom(z => z.Surname))
+                .ForMember(x => x.Pictures, z => z.MapFrom(z => z.Pictures))
+                .ReverseMap();
+
+            CreateMap<Admin, AdminModel>()
+                .ForMember(x => x.Name, z => z.MapFrom(z => z.Name))
+                .ForMember(x => x.Surname, z => z.MapFrom(z => z.Surname))
+                .ReverseMap();
+
+            CreateMap<Picture, PictureModel>()
+                .ForMember(x => x.Name, z => z.MapFrom(z => z.Name))
+                .ForMember(x => x.Path, z => z.MapFrom(z => z.Path))
+                .ForMember(x => x.Path, z => z.MapFrom(z => z.Path))
+                .ForMember(x => x.Description, z => z.MapFrom(z => z.Description))
+                .ForMember(x => x.Tags, z => z.MapFrom(z => z.Tags))
+                .ReverseMap();
+
+            CreateMap<Tag, TagModel>()
+                .ForMember(x => x.NameTag, z => z.MapFrom(z => z.NameTag))
+                .ReverseMap();
+        }
+    }
+}
